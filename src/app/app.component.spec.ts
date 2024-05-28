4755f9deefd301193b6f60f7270149c70aa03391
WilberElias <ia.wilberelias@ufg.edu.sv>
2024-05-28 10:38:37 -0600

    Actualizar estilos del componente de modal de tarjeta

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -46,13 +46,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app.title).toEqual('FiruPets');
   });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain(
-      'Hello, FiruPets'
-    );
-  });
 });

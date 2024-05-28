d2501c3d27b1f815513cca955e174716df9532fb
WilberElias <ia.wilberelias@ufg.edu.sv>
2024-05-28 11:08:36 -0600

    Actualizar importaciones y declaraciones en las especificaciones del componente de la aplicación

--- src/app/sidebar/sidebar.component.spec.ts
+++ src/app/sidebar/sidebar.component.spec.ts
@@ -1,14 +1,28 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { SidebarComponent } from './sidebar.component';
+import { PublicacionesService } from '../publicaciones.service';
+import { Firestore, provideFirestore } from '@angular/fire/firestore';
+import { AuthService } from '../auth.service';
+import { Auth, provideAuth } from '@angular/fire/auth';
+import { FormsModule } from '@angular/forms'; // Importa FormsModule
 
 describe('SidebarComponent', () => {
   let component: SidebarComponent;
   let fixture: ComponentFixture<SidebarComponent>;
 
   beforeEach(async () => {
+    const mockFirestore = {}; // Crea un objeto simulado para Firestore
+    const mockAuth = {}; // Crea un objeto simulado para Auth
+
     await TestBed.configureTestingModule({
-      declarations: [SidebarComponent]
+      declarations: [ SidebarComponent ],
+      imports: [ FormsModule ], // Añade FormsModule a la lista de imports
+      providers: [
+        PublicacionesService,
+        AuthService,
+        { provide: Firestore, useValue: mockFirestore }, // Proporciona el objeto simulado para Firestore
+        { provide: Auth, useValue: mockAuth } // Proporciona el objeto simulado para Auth
+      ]
     })
     .compileComponents();
     

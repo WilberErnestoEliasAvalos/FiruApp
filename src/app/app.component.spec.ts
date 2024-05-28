b35935f497e1c0f6f007c5bb75ad014530fbc0db
WilberElias <ia.wilberelias@ufg.edu.sv>
2024-05-28 10:27:10 -0600

    Actualizar importaciones y declaraciones en las especificaciones del componente de la aplicación

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,16 +1,37 @@
 import { TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
+import { RouterTestingModule } from '@angular/router/testing'; // Importa RouterTestingModule
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { AuthService } from './auth.service'; // Importa AuthService
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { of } from 'rxjs';
+import { EventEmitter } from '@angular/core';
+
+
+// Creamos un mock para Auth
+class MockAuth {}
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule, // Agrega RouterTestingModule a los imports
       ],
-      declarations: [
-        AppComponent
+      declarations: [AppComponent, NavbarComponent],
+      providers: [
+        { 
+          provide: AuthService, 
+          useValue: { 
+            authState$: of({ /* Proporciona un usuario simulado */ }), 
+            loginSuccess: new EventEmitter<void>(), 
+            logoutSuccess: new EventEmitter<void>(), 
+            getAuth: () => ({}), 
+            login: () => Promise.resolve(), 
+            logout: () => Promise.resolve()
+          } 
+        },
       ],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA]
     }).compileComponents();
   });
 
@@ -30,6 +51,8 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, FiruPets');
+    expect(compiled.querySelector('h1')?.textContent).toContain(
+      'Hello, FiruPets'
+    );
   });
 });

ae73ffc3927b7c15ab6e60ab0e9a9122da112b97
WilberElias <ia.wilberelias@ufg.edu.sv>
2024-05-28 11:02:12 -0600

    Actualizar importaciones y declaraciones en las especificaciones del componente de la aplicación

--- src/app/signup/signup.component.spec.ts
+++ src/app/signup/signup.component.spec.ts
@@ -1,16 +1,28 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { SignupComponent } from './signup.component';
 import { Auth } from '@angular/fire/auth';
+import { Firestore } from '@angular/fire/firestore';
+import { FirebaseStorage, Storage } from '@angular/fire/storage';
+import { MatIconModule } from '@angular/material/icon';
+import { ReactiveFormsModule } from '@angular/forms'; // Importa ReactiveFormsModule
 
 describe('SignupComponent', () => {
   let component: SignupComponent;
   let fixture: ComponentFixture<SignupComponent>;
 
   beforeEach(async () => {
+    const mockFirebaseStorage = {} as FirebaseStorage;
+
     await TestBed.configureTestingModule({
       declarations: [ SignupComponent ],
+      imports: [ 
+        MatIconModule, 
+        ReactiveFormsModule // Agrega ReactiveFormsModule a la lista de imports
+      ],
       providers: [
-        { provide: Auth, useValue: {} } // Proporcionamos un valor para Auth
+        { provide: Auth, useValue: {} },
+        { provide: Firestore, useValue: {} },
+        { provide: Storage, useValue: mockFirebaseStorage }
       ]
     })
     .compileComponents();

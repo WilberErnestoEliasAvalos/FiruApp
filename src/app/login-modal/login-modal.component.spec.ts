41761e3a40140f19a97fd26f59a141da513a78b2
WilberElias <ia.wilberelias@ufg.edu.sv>
2024-05-28 11:04:47 -0600

    Actualizar importaciones y declaraciones en las especificaciones del componente de la aplicación

--- src/app/login-modal/login-modal.component.spec.ts
+++ src/app/login-modal/login-modal.component.spec.ts
@@ -1,14 +1,25 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { LoginModalComponent } from './login-modal.component';
+import { Auth } from '@angular/fire/auth';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router'; // Importa RouterModule
 
 describe('LoginModalComponent', () => {
   let component: LoginModalComponent;
   let fixture: ComponentFixture<LoginModalComponent>;
 
   beforeEach(async () => {
+    const mockAuth = {};
+
     await TestBed.configureTestingModule({
-      declarations: [LoginModalComponent]
+      declarations: [ LoginModalComponent ],
+      imports: [ 
+        FormsModule,
+        RouterModule.forRoot([]) // Agrega RouterModule a la lista de imports
+      ],
+      providers: [
+        { provide: Auth, useValue: mockAuth }
+      ]
     })
     .compileComponents();
     
